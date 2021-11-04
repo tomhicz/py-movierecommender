a2165943b036ae3d78e8fa4b7cf9e51bccd3f3bf
Tom Hickz <999194+tomhicz@users.noreply.github.com>
2021-11-04 21:08:38 +0900

    try to get runing on heroku

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask,request,jsonify
 #from flask_cors import CORS
-from recommender import recommender
+#from recommender import recommender
 
 
 app = Flask(__name__)
 #CORS(app) 
+
+@app.route("/")
+def home():
+    return "Hello World! I'm using Flask."
         
 @app.route('/movie', methods=['GET'])
 def recommend_movies():
-    res = recommender()
+    #res = recommender()
     #res = recommender(request.args.get('title'))
     return jsonify(res)
 
